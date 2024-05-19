17d880398f1c1e0adda164e6ce1fcbbe3838bea6
akshaymalik1995 <akshaymalik191@gmail.com>
2024-05-19 12:42:45 +0530

    configuration changes for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/akshaymalik1995",
+  base: "https://github.com/akshaymalik1995",
   plugins: [react()],
 })
